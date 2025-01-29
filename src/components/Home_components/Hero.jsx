d654ec116f6117caf035faef891416b096c1f7aa
Simon Ddungu <dev.simonddungu@gmail.com>
2025-01-30 01:17:56 +0300

    minor changes to hero section, added a Menu button

--- src/components/Home_components/Hero.jsx
+++ src/components/Home_components/Hero.jsx
@@ -10,7 +10,7 @@ import NavigationBar from "./NavigationBar";
 const Hero = () => {
 
     return (
-        <div className="md:h-screen bg-[#dadccc] overflow-hidden" >
+        <div className="md:min-h-screen bg-[#dadccc] overflow-hidden p-0" >
 
         <NavigationBar />
 
@@ -19,27 +19,19 @@ const Hero = () => {
 
                 <div>
                     <h1 className="playfair-display-Font text-4xl mb-5 md:mb-10">From Our Chef's Imagination to Your Plate, <br />Experience the Extraordinary</h1>
-                    <p className="mb-10 md:mb-15 lora-Font">Every dish we serve is a masterpiece, meticulously crafted to delight your senses and elevate your dining experience. With the finest ingredients and innovative techniques, we bring you a journey that transcends the ordinary. Join us and indulge in a moment of true culinary artistry.</p>
+                    <p className="mb-10 md:mb-10 Lora-Font">Every dish we serve is a masterpiece, meticulously crafted to delight your senses and elevate your dining experience. With the finest ingredients and innovative techniques, we bring you a journey that transcends the ordinary. Join us and indulge in a moment of true culinary artistry.</p>
 
-                    <a href="#" className="rounded-full bg-[#002816] px-5 md:px-10 py-3 md:py-5 text-white">Make a Reservation</a>
+                    <div className="flex items-center gap-3 md:gap-5">
+                        <a href="#" className="rounded-full bg-[#153B44] px-5 md:px-7 py-3 md:py-4 text-white hover:bg-(--dark-green) transition-all ease-in-out">Make a Reservation</a>
+                        <a href="#" className="rounded-full bg-white border-3 border-(--lime-green) px-5 md:px-7 py-3 md:py-4 hover:bg-black hover:text-white hover:border-black transition-all ease-in-out">Our Menu</a>
+                    </div>
                 </div>
             
-                <div className="relative w-1xl ">
-                    <div className="z-50 w-80 md:w-125">
+                
+                    <div className="w-100 md:w-500">
                         <img  className="w-full" src={HeroImage} alt="Chips Meal" />
                     </div>
-                    
-                    {/*
-                    <div className="absolute -top-3 left-10 z-10 w-60 rounded-full bg-[#002816]">
-                        <img className="w-full" src={Food_item2} alt="Chips Meal" />
-                    </div>
-
-                    <div className="absolute bottom-10 right-10 z-30 w-70 rounded-full bg-[#002816]">
-                        <img className="w-full" src={Food_item3} alt="Chips Meal" />
-                    </div>
-                    */}
-                    
-                </div>
+               
 
                 
 
