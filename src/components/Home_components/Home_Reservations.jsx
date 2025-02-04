cb9d843eef4e9e7a16e7fbe1af17b728e1582a7c
Simon Ddungu <dev.simonddungu@gmail.com>
2025-02-04 03:57:06 +0300

    Minor Changes

--- src/components/Home_components/Home_Reservations.jsx
+++ src/components/Home_components/Home_Reservations.jsx
@@ -14,7 +14,7 @@ const HomeReservations = () => {
                 <h5 className="text-center playfair-display-Font text-2xl md:text-4xl mb-5 md:mb-7">Your Table Awaits</h5>
                 <p className="mb-5">Great food deserves great company and we'd love to have you! Whether you're planning a romantic dinner, a family gathering or a casual night out, booking your table ensures you have the perfect spot waiting for you. Skip the wait, relax and enjoy a seamless dining experience at Celesti Restaurant.</p>
                 <p className='mb-5 md:mb-10'>We recommend making a reservation in advance, especially during weekends and peak hours. </p>
-                <Link to ="/under-construction" className="block w-max mx-auto rounded-full bg-[#153B44] px-5 md:px-7 py-3 md:py-4 text-white hover:bg-(--dark-green) transition-all ease-in-out">Book a Table</Link>
+                <Link to ="/reservation" className="block w-max mx-auto rounded-full bg-[#153B44] px-5 md:px-7 py-3 md:py-4 text-white hover:bg-(--dark-green) transition-all ease-in-out">Book a Table</Link>
             </div>
         </div>
 
